875b7a65f1c6e0540b8948bd2ddbd791359c6daa
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-18 10:38:24 +0500

    News api called

--- app/src/main/java/com/muhsantech/newsappkotlin/MainActivity.kt
+++ app/src/main/java/com/muhsantech/newsappkotlin/MainActivity.kt
@@ -2,20 +2,25 @@ package com.muhsantech.newsappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView : RecyclerView
+    lateinit var adapter : NewsAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView = findViewById(R.id.newsList)
 
 //        val songs = listOf("Hell,", "Muhsan", "List", "Life", "Qunation")
 
-        val songsObject = mutableListOf<Song>()
+        /*val songsObject = mutableListOf<Song>()
         songsObject.add(Song("Hello","Just the Description"))
         songsObject.add(Song("Hello","Just the Description"))
         songsObject.add(Song("Hello","Just the Description"))
@@ -25,8 +30,30 @@ class MainActivity : AppCompatActivity() {
         songsObject.add(Song("Hello","Just the Description"))
         songsObject.add(Song("Hello","Just the Description"))
         songsObject.add(Song("Hello","Just the Description"))
-        songsObject.add(Song("Hello","Just the Description"))
-        recyclerView.adapter = MyAdapter(songsObject)
-        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        songsObject.add(Song("Hello","Just the Description"))*/
+//        recyclerView.adapter = MyAdapter(songsObject)
+//        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+
+        getNews()
+    }
+
+    private fun getNews() {
+        val news = NewsService.newsInstance.getHeadLines("in",1)
+        news.enqueue(object : Callback<News> {
+            override fun onResponse(call: Call<News>, response: Response<News>) {
+                val news = response.body()
+                if (news != null){
+                    Log.d("MUHSANTECH", news.toString())
+                    adapter = NewsAdapter(this@MainActivity, news.articles)
+                   recyclerView.adapter = adapter
+                   recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+                }
+            }
+
+            override fun onFailure(call: Call<News>, t: Throwable) {
+                Log.d("MUHSANTECH","Error in Fetching NEws")
+            }
+
+        })
     }
 }
