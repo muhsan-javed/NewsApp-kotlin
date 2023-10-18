96e83bd3fa797acd88376305fea3f89686620fbd
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-18 09:36:39 +0500

    Setup Recycler View DataClass

--- app/src/main/java/com/muhsantech/newsappkotlin/MainActivity.kt
+++ app/src/main/java/com/muhsantech/newsappkotlin/MainActivity.kt
@@ -2,10 +2,31 @@ package com.muhsantech.newsappkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+
+//        val songs = listOf("Hell,", "Muhsan", "List", "Life", "Qunation")
+
+        val songsObject = mutableListOf<Song>()
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        songsObject.add(Song("Hello","Just the Description"))
+        recyclerView.adapter = MyAdapter(songsObject)
+        recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
     }
 }
---
+++ app/src/main/java/com/muhsantech/newsappkotlin/Song.kt
@@ -0,0 +1,3 @@
+package com.muhsantech.newsappkotlin
+
+data class Song(val title:String, val description:String)
